61772d21da861d1b67ce40f5ab7bf8cbaf425bf4
Pro <plateau93@gmail.com>
2020-04-26 05:35:01 +0200

    commit acceuil+mail

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -13,6 +13,9 @@ import { Account } from 'app/core/user/account.model';
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
+  lat = 48.9321269;
+  lng = 2.4015819;
+  titre: any;
 
   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
 
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,16 @@ import { RouterModule } from '@angular/router';
 import { Plateau93SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  imports: [Plateau93SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [
+    Plateau93SharedModule,
+    RouterModule.forChild([HOME_ROUTE]),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDj-zgI5H5vSaR9NbLwk7BxCyPiCz3cCTs'
+    })
+  ],
   declarations: [HomeComponent]
 })
 export class Plateau93HomeModule {}
