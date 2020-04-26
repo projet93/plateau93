4a48e67fed280d5c4174872260d58a3398f23b93
spring <plateau93@gmail.com>
2020-04-26 19:57:42 +0200

    gestion authentification

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+
+import { ClubService } from 'app/entities/club/club.service';
+import { IClub, Club } from 'app/shared/model/club.model';
 
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
@@ -16,14 +20,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   lat = 48.9321269;
   lng = 2.4015819;
   titre: any;
-
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  club?: IClub | null = null;
+  constructor(private accountService: AccountService,protected clubService: ClubService, private loginModalService: LoginModalService) {}
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+    this.clubService.findByUser().subscribe((res: HttpResponse<IClub>) => (this.club = res.body || new Club()));
   }
 
   isAuthenticated(): boolean {
+    if(this.accountService.isAuthenticated()){
+      this.titre = this.club?.nom;
+    }
     return this.accountService.isAuthenticated();
   }
 
